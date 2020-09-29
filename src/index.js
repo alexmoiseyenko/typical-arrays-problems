e2c4be76a6dc4a80e7e05d5ead49b4e21259eddd
Alex <beldeut@gmail.com>
2020-09-29 15:50:30 +0300

    feat: add solutions for max, min and avr func

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (!array || !array.length) {
+    return 0;
+  }
+
+  let minNum = array[0];
+
+  array.forEach((item, idx) => {
+  for (let i = idx; i < array.length; i++) {
+      if (array[i] < minNum) {
+          minNum = array[i];
+      }
+    }
+  });
+  return minNum;
+};
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length) {
+    return 0;
+  }
+
+  let maxNum = array[0];
+
+  array.forEach((item, idx) => {
+    for (let i = idx; i < array.length; i++) {
+        if (array[i] > maxNum) {
+            maxNum = array[i];
+        }
+    }
+  });
+  return maxNum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || !array.length) {
+    return 0;
+  }
+
+  let sum = array.reduce((acc, item) => acc + item);
+  return sum/array.length;
 }
